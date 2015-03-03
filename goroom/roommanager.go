89283a9b7976fe264807bb3ce7889338849a5fb4
Arjan Topolovec <arjan.top@gmail.com>
2015-03-03 13:27:03 +0100

    Use go generate to generate protocol definitions.

--- goroom/roommanager.go
+++ goroom/roommanager.go
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+//go:generate protoc --go_out=plugins=grpc:. -I ../protos ../protos/goroom.proto
+
 package goroom
 
 import (
